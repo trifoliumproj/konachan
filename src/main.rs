92809204c16b6956c2f2c68c2534c3ff951964fa
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-24 10:42:05 +0700

    feat(main.rs): add support for multiple tags

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,21 @@ impl api_server::Api for APIImplementation {
         request: tonic::Request<GalleryRequest>,
     ) -> Result<tonic::Response<GalleryResponse>, tonic::Status> {
         let req = request.into_inner();
-        let tag = req.tags.get(0);
+        let tags = req.tags;
+
+        if tags.len() == 0 {
+            return Err(tonic::Status::failed_precondition(
+                "Tags should not be empty.",
+            ));
+        }
+
+        let mut n: Vec<String> = Vec::new();
+        for tag in &tags {
+            n.push(tag.name.clone());
+        }
+
         let mut url = reqwest::Url::parse("https://konachan.com/post.json").unwrap();
-        let _ = match tag {
-            Some(unwrapped_tag) => {
-                let tag_name = unwrapped_tag.name.clone();
-                url.query_pairs_mut().append_pair("tags", &tag_name);
-            }
-            None => {
-                return Err(tonic::Status::failed_precondition(
-                    "Tags should not be empty.",
-                ));
-            }
-        };
+        url.query_pairs_mut().append_pair("tags", &(n.join(" ")));
         let mut ret = GalleryResponse { posts: [].to_vec() };
         let resp = reqwest::blocking::get(url);
         let _ = match resp {
